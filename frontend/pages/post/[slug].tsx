ca31515f815242ed93a4e497c4f9b6c07dc27cd4
intgus <46197948+intGus@users.noreply.github.com>
2023-03-30 10:48:13 -0400

    Add author and category labels in post/slug file

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -1,19 +1,32 @@
+import groq from 'groq'
 import { client } from '../../client'
 
 const Post = ({post}) => {
-  
+  const { title = 'Missing title', name = 'Missing name', categories } = post
   return (
     <article>
-      <h1>{post?.slug?.current}</h1>
+      <h1>{title}</h1>
+      <span>By {name}</span>
+      {categories && (
+        <ul>
+          Posted in
+          {categories.map(category => <li key={category}>{category}</li>)}
+        </ul>
+      )}
     </article>
   )
 }
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title
+}`
 
 export async function getStaticPaths() {
   const paths = await client.fetch(
     `*[_type == "post" && defined(slug.current)][].slug.current`
   )
-
   return {
     paths: paths.map((slug) => ({params: {slug}})),
     fallback: true,
@@ -23,10 +36,7 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = '' } = context.params
-  const post = await client.fetch(`
-    *[_type == "post" && slug.current == $slug][0]
-  `, { slug })
-  
+  const post = await client.fetch(query, { slug })
   return {
     props: {
       post
