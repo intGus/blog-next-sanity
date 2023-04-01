1bcaa4dfee0c8b9d1736551f817b5e27330b4142
intgus <46197948+intGus@users.noreply.github.com>
2023-04-01 18:36:06 -0400

    Add posts bodies to the slug file

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -1,8 +1,37 @@
 import groq from 'groq'
+import imageUrlBuilder from '@sanity/image-url'
+import {PortableText} from '@portabletext/react'
 import { client } from '../../client'
 
+function urlFor (source) {
+  return imageUrlBuilder(client).image(source)
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value }) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <img
+          alt={value.alt || ' '}
+          loading="lazy"
+          src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+        />
+      )
+    }
+  }
+}
+
 const Post = ({post}) => {
-  const { title = 'Missing title', name = 'Missing name', categories } = post
+  const { 
+    title = 'Missing title',
+    name = 'Missing name',
+    categories,
+    authorImage,
+    body = []
+  } = post
   return (
     <article>
       <h1>{title}</h1>
@@ -13,6 +42,20 @@ const Post = ({post}) => {
           {categories.map(category => <li key={category}>{category}</li>)}
         </ul>
       )}
+      {authorImage && (
+        <div>
+          <img
+            src={urlFor(authorImage)
+              .width(50)
+              .url()}
+            alt={`${name}'s picture`}
+          />
+        </div>
+      )}
+      <PortableText
+        value={body}
+        components={ptComponents}
+      />
     </article>
   )
 }
@@ -20,7 +63,9 @@ const Post = ({post}) => {
 const query = groq`*[_type == "post" && slug.current == $slug][0]{
   title,
   "name": author->name,
-  "categories": categories[]->title
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  body
 }`
 
 export async function getStaticPaths() {
