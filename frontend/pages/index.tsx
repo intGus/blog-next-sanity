3f360cd13b5be752f285e72e095bf4da533eca9b
intgus <46197948+intGus@users.noreply.github.com>
2023-04-01 20:34:03 -0400

    Updat the index to include a posts summary

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,36 @@
-const Index = () => {
-  return (
-    <div>
-      <p>Hello world!</p>
-    </div>
-  )
+import Link from 'next/link'
+import groq from 'groq'
+import { client } from '../client'
+
+const Index = ({posts}) => {
+    return (
+      <div>
+        <h1>Welcome to a blog!</h1>
+        {posts.length > 0 && posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href={`/post/${encodeURIComponent(slug.current)}`}>
+                  {title}
+                  ({new Date(publishedAt).toDateString()})
+                </Link>{' '}
+              </li>
+            )
+        )}
+      </div>
+    )
 }
 
-export default Index;
+export async function getStaticProps() {
+    const posts = await client.fetch(groq`
+      *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+    `)
+    console.log(posts)
+    return {
+      props: {
+        posts
+      }
+    }
+}
+
+export default Index
