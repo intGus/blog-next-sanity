cfc06d598ca6030756e45df9233939ca948220e6
intgus <46197948+intGus@users.noreply.github.com>
2023-04-02 22:26:30 -0400

    test the new config with cloudflare pages

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  // output: 'export',
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
